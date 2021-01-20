b6b001e8983d26f71ca2389e5bec07d11a6623d4
Giada Sacchi <71716411+giada-sacchi@users.noreply.github.com>
2021-01-20 19:19:54 +0000

    Update MH_temp_fun.R

--- MH_temp_fun.R
+++ MH_temp_fun.R
@@ -1,17 +1,3 @@
-#### Packages & Scripts ####
-if(!require(boot)) install.packages("boot")
-library(boot)
-if(!require(Rcpp)) install.packages("Rcpp")
-library(Rcpp)
-if(!require(RcppArmadillo)) install.packages("RcppArmadillo")
-library(RcppArmadillo)   
-if(!require(invgamma)) install.packages("invgamma")
-library(invgamma)
-if(!require(numbers)) install.packages("numbers")
-library(numbers) # mod
-sourceCpp("13253_2017_282_MOESM4_ESM.cpp")
-
-
 ##### UPDATEPAR.TEMP #####
 # Function used to update parameters (acceptance prob computed considering chain temperature)
 
@@ -94,7 +80,6 @@ updatepar.temp <- function(par.list, npar, data,
   output
 }
 
-
 ##### PRIOR #####
 # Function to compute the sum of logprior distributions of a parameter vector
 
@@ -111,5 +96,5 @@ prior <- function(par.list, mu, sigma, alpha, beta, shape, rate){
         }
     }
   }
-  prior
+  return(prior)
 }
